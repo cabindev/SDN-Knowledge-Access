46d556a63f9921d0279db75888907667aa4c2324
deliverzd <tomrto52@gmail.com>
2024-03-17 02:19:27 +0700

    Revert "Revert "Revert "Revert "Revert "Revert "trya add auth system""""""
    This reverts commit d3f4dc65a984a0055a942c48bae1eb4e6dc71e38.

---
+++ src/actions/member/update.ts
@@ -0,0 +1,34 @@
+"use server";
+
+import * as yup from "yup";
+import prisma from "@/utils/prisma";
+import { FormState } from "@/types";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { validate } from "@/utils/validate";
+
+const schema = yup.object().shape({
+    id: yup.string().required(),
+    email: yup.string().email().required(),
+    role: yup.string().oneOf(["normal", "manager"]).required(),
+});
+
+export async function updateMember(prevState: FormState, formData: FormData): Promise<FormState> {
+    const data = {
+        id: formData.get("id") as string,
+        email: formData.get("email") as string,
+        role: formData.get("role") as any,
+    };
+
+    const { errors } = await validate(schema, data);
+    if (errors) return { error: errors };
+
+    try {
+        await prisma.member.update({ where: { id: data.id }, data });
+    } catch (err) {
+        return { error: ["something went wrong"] };
+    }
+
+    revalidatePath("/manager/member/");
+    redirect("/manager/member/");
+}
